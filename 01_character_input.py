d2a99605c6981cde97d32935f0319204141be1ee
Andrew MacDonald <ammacdonald3@gmail.com>
2018-08-30 15:29:28 -0400

    Fixed defect by changing year value to INT

--- 01_character_input.py
+++ 01_character_input.py
@@ -2,6 +2,6 @@ import time
 name = input("Please enter your name. ")
 age = int(input("Please enter your age. "))
 
-year_100 = 100 - age + (time.strftime("%Y"))
+year_100 = 100 - age + int((time.strftime("%Y")))
 
 print(f"Thanks, {name}! You're currently {age} years old, and you're going to turn 100 during Year {year_100}.")
