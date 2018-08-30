ad2268a3b465c85df9da5bfba9226ce3f3b2e628
Andrew MacDonald <ammacdonald3@gmail.com>
2018-08-30 14:53:36 -0400

    Added Character Input file

---
+++ 01_character_input.py
@@ -0,0 +1,8 @@
+
+name = input("Please enter your name. ")
+age = input("Please enter your age. ")
+age = int(age)
+
+year_100 = 100 - age + 2018
+
+print(f"Thanks, {name}! You're currently {age} years old, and you're going to turn 100 during Year {year_100}.")
